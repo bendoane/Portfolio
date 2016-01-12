139ed9fbe1f3b287eafccb08cac03de4f0c447cd
bendoane <ben.doane@gmail.com>
2016-01-12 15:57:23 -0500

    fixing image loads

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,10 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
+
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
